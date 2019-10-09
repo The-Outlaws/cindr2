131cd85daac76747ab20a64ac85208d9e420c7f1
Julia Catalano <jcatalano1990@gmail.com>
2019-10-09 16:12:36 -0400

    add: workaround switching scenes

--- client/game/scenes/questionRoom.js
+++ client/game/scenes/questionRoom.js
@@ -1,18 +1,18 @@
-import Phaser from 'phaser'
-import Avatar from '../sprites/Avatar'
+import Phaser from 'phaser';
+import Avatar from '../sprites/Avatar';
 //import Background from '../sprites/Background'
 
 export default class QuestionRoom extends Phaser.Scene {
   constructor() {
-    super({key: 'QuestionRoom'})
+    super({ key: 'QuestionRoom' });
   }
   init() {
-    this.playerSpeed = 10
+    this.playerSpeed = 10;
   }
 
   preload() {
     //this.load.image('crystalBackground', '/CrystalScene.png')
-    this.load.image('troll', '/troll128.png')
+    this.load.image('troll', '/troll128.png');
   }
 
   create() {
@@ -24,46 +24,47 @@ export default class QuestionRoom extends Phaser.Scene {
     //height: this.game.config.height,
     //asset: 'crystalBackground'
     //})
-    this.add.text(450, 450, 'Question question?')
-    this.add.text(100, 600, 'Anser Anser')
-    this.add.text(300, 600, 'Anwer Anwer')
+    this.add.text(450, 450, 'Question question?');
+    this.add.text(100, 600, 'Anser Anser');
+    this.add.text(300, 600, 'Anwer Anwer');
 
     this.avatar = new Avatar({
       scene: this,
       x: 100,
       y: 700,
       asset: 'troll'
-    })
-    this.add.existing(this.avatar)
+    });
+    this.add.existing(this.avatar);
     //this.add.existing(this.bg)
 
-    this.cursors = this.input.keyboard.createCursorKeys()
+    this.cursors = this.input.keyboard.createCursorKeys();
   }
 
   update() {
     if (this.cursors.left.isDown) {
-      if (this.cursors.up.isDown) this.avatar.y -= this.playerSpeed
-      else if (this.cursors.down.isDown) this.avatar.y += this.playerSpeed
+      if (this.cursors.up.isDown) this.avatar.y -= this.playerSpeed;
+      else if (this.cursors.down.isDown) this.avatar.y += this.playerSpeed;
 
-      this.avatar.x -= this.playerSpeed
+      this.avatar.x -= this.playerSpeed;
     }
     if (this.cursors.right.isDown) {
-      if (this.cursors.up.isDown) this.avatar.y -= this.playerSpeed
-      else if (this.cursors.down.isDown) this.avatar.y += this.playerSpeed
+      if (this.cursors.up.isDown) this.avatar.y -= this.playerSpeed;
+      else if (this.cursors.down.isDown) this.avatar.y += this.playerSpeed;
 
-      this.avatar.x += this.playerSpeed
+      this.avatar.x += this.playerSpeed;
     }
     if (this.cursors.up.isDown) {
-      if (this.cursors.left.isDown) this.avatar.x -= this.playerSpeed
-      else if (this.cursors.right.isDown) this.avatar.x += this.playerSpeed
+      if (this.cursors.left.isDown) this.avatar.x -= this.playerSpeed;
+      else if (this.cursors.right.isDown) this.avatar.x += this.playerSpeed;
 
-      this.avatar.y -= this.playerSpeed
+      this.avatar.y -= this.playerSpeed;
     }
     if (this.cursors.down.isDown) {
-      if (this.cursors.left.isDown) this.avatar.x -= this.playerSpeed
-      else if (this.cursors.right.isDown) this.avatar.x += this.playerSpeed
+      if (this.cursors.left.isDown) this.avatar.x -= this.playerSpeed;
+      else if (this.cursors.right.isDown) this.avatar.x += this.playerSpeed;
 
-      this.avatar.y += this.playerSpeed
+      this.avatar.y += this.playerSpeed;
     }
+    this.input.on('pointerdown', () => this.scene.start('DestinationRoom'));
   }
 }
