88b28e2376da3c466208c1254871dd9c546676d7
Kimberly Zawacki <ximberlyzawacki@gmail.com>
2019-10-18 11:05:24 -0400

    feat: dynamic instructions - instructions that change as you move

--- client/game/scenes/Splash.js
+++ client/game/scenes/Splash.js
@@ -11,21 +11,30 @@ export default class Splash extends Phaser.Scene {
 
   init() {
     this.playerSpeed = 10;
+    this.usedCursor = false;
   }
 
   preload() {
     const { user: { avatar } } = store.getState();
     this.load.image(avatarStr, avatar);
     this.load.image('arrows', 'arrowkeys.png');
+    //this.load.image('graaass', 'graaass.png')
   }
 
   create() {
     this.backgroundColor = 'black';
+    this.bg = this.add.image(
+      this.game.config.width / 2,
+      this.game.config.height / 2,
+      'starryNight'
+    );
+    this.bg.displayWidth = this.game.config.width;
+    this.bg.displayHeight = this.game.config.height;
 
-    this.add.text(
-      1.57 * this.game.config.width / 4,
+    this.instructions = this.add.text(
+      this.game.config.width / 6,
       1.1 * this.game.config.height / 4,
-      'Move your avatar with \nup/down arrow keys\nMove into text to make selection',
+      'Use arrow keys to move your avatar',
       {
         fill: 'white',
         font: '4em Walter Turncoat',
@@ -42,32 +51,33 @@ export default class Splash extends Phaser.Scene {
         font: '6em Walter Turncoat'
       }
     );
-    const line = new Phaser.Geom.Line(
-      this.game.config.width / 6,
-      this.game.config.height / 5,
-      this.game.config.width / 6,
-      3 * this.game.config.height / 4
-    );
-    const line2 = new Phaser.Geom.Line(
-      this.game.config.width / 6,
-      3 * this.game.config.height / 4 + 20,
-      2.6 * this.game.config.width / 4 - 20,
-      3 * this.game.config.height / 4 + 20
-    );
-    var graphics = this.add.graphics({
-      lineStyle: { width: 20, color: 0xffffff }
-    });
+    // const line = new Phaser.Geom.Line(
+    //   this.game.config.width / 6,
+    //   this.game.config.height / 5,
+    //   this.game.config.width / 6,
+    //   3 * this.game.config.height / 4
+    // );
+    // const line2 = new Phaser.Geom.Line(
+    //   this.game.config.width / 6,
+    //   3 * this.game.config.height / 4 + 20,
+    //   2.6 * this.game.config.width / 4 - 20,
+    //   3 * this.game.config.height / 4 + 20
+    // );
+    // var graphics = this.add.graphics({
+    //   lineStyle: { width: 10, color: 0xffffff }
+    // });
 
-    graphics.strokeLineShape(line);
-    graphics.strokeLineShape(line2);
+    // graphics.strokeLineShape(line);
+    // graphics.strokeLineShape(line2);
 
     this.physicsObjectStart = this.physics.add.existing(this.start, 'static');
 
     this.avatar = this.physics.add.sprite(
       this.game.config.width / 6,
-      this.game.config.height / 5,
+      3 * this.game.config.height / 4,
       avatarStr
     );
+
     this.avatar.body.setAllowGravity(false);
     this.avatar.setCollideWorldBounds(true);
 
@@ -80,15 +90,38 @@ export default class Splash extends Phaser.Scene {
       null,
       this
     );
-    this.add.image(
-      this.game.config.width / 2 + 100,
-      this.game.config.height / 2 + 40,
-      'arrows'
-    );
+    // this.add.image(
+    //   this.game.config.width / 2 + 100,
+    //   this.game.config.height / 2 + 40,
+    //   'arrows'
+    // );
     this.cursors = this.input.keyboard.createCursorKeys();
   }
 
   update() {
+    if (
+      this.cursors.left.isDown ||
+      this.cursors.right.isDown ||
+      this.cursors.up.isDown ||
+      this.cursors.down.isDown
+    ) {
+      this.usedCursor = true;
+    }
+
+    if (this.usedCursor) {
+      this.instructions.destroy();
+      this.add.text(
+        this.game.config.width / 6,
+        1.1 * this.game.config.height / 4,
+        'To start your adventure, glide into "Start Adventure" portal',
+        {
+          fill: 'white',
+          font: '4em Walter Turncoat',
+          align: 'center'
+        }
+      );
+    }
+
     if (this.cursors.left.isDown) {
       if (this.cursors.up.isDown) this.avatar.y -= this.playerSpeed;
       else if (this.cursors.down.isDown) this.avatar.y += this.playerSpeed;
