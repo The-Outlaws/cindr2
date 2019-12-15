d32722ebd1fe87fdf21d30e24fbeaeaec00d8d34
petradish <49966727+petradish@users.noreply.github.com>
2019-12-14 20:35:03 -0500

    Delete: superfluous code

--- server/socket/index.js
+++ server/socket/index.js
@@ -1,6 +1,3 @@
-const { Message, Conversation } = require('../db/models');
-const mobileSockets = {};
-
 module.exports = io => {
   io.on('connection', socket => {
     console.log(
@@ -10,21 +7,7 @@ module.exports = io => {
     socket.on('new-message', message => {
       socket.broadcast.emit('new-message', message);
     });
-    socket.on('message', ({ text, sender, receiver }) => {
-      Message.createMessage(text, sender, receiver).then(message => {
-        socket.emit('incomingMessage', message);
-        const receiverSocketId = mobileSockets[receiver.id];
-        socket.to(receiverSocketId).emit('incomingMessage', message);
-      });
-    });
-    socket.on('chat', users => {
-      Conversation.findOrCreateConversation(
-        users.user.id,
-        users.receiver.id
-      ).then(conversation =>
-        socket.emit('priorMessages', conversation.messages)
-      );
-    });
+   
     socket.on('disconnect', () => {
       console.log(`Connection ${socket.id} has left the building`);
     });
