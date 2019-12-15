01cfaf05519147e48b1d71f871e781c53dd84c32
petradish <49966727+petradish@users.noreply.github.com>
2019-12-14 20:22:08 -0500

    Refactor: .find for filteredConvo
    Fix: socket dispatching "getMessage" which multiplied number of messages rendering for receiving user

--- client/components/messagesList.js
+++ client/components/messagesList.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
-import Message from './Message';
 import { connect } from 'react-redux';
 import { withRouter } from 'react-router-dom';
+import moment from 'moment';
+
+import Message from './Message';
 import Chat from './newMessageEntry';
 import { MatchRequest } from './matchRequest';
 import {
@@ -9,7 +11,7 @@ import {
   acceptConversation,
   rejectConversation
 } from '../store';
-import moment from 'moment';
+
 
 class disconnectedMessagesList extends Component {
   constructor(props) {
@@ -38,20 +40,20 @@ class disconnectedMessagesList extends Component {
   }
   render() {
     const allConversations = this.props.conversations;
-    const matchId = Number(this.props.match.params.matchId); // because it's a string "1", not a number!
-    const filteredConvo = allConversations.filter(
+    const matchId = Number(this.props.match.params.matchId);
+    const filteredConvo = allConversations.find(
       convo => convo.matchId === matchId || convo.userId === matchId
     );
-    const uniqueMessages = [...new Set(this.props.messages)];
-    const currMessages = uniqueMessages.filter(message => {
-      return message.conversationId === filteredConvo[0].id;
+    const currMessages = this.props.messages.filter(message => {
+      return message.conversationId === filteredConvo.id;
     });
+
     const today = moment();
-    console.log('currMessages', currMessages);
+
     return (
       <main>
-        {filteredConvo.length ? (
-          filteredConvo[0].isAccepted ? (
+        {filteredConvo ?
+          filteredConvo.isAccepted ? (
             <div>
               <h4>Recent Messages</h4>
               <ul className="media-list">
@@ -63,9 +65,9 @@ class disconnectedMessagesList extends Component {
                   />
                 ))}
               </ul>
-              <Chat conversationId={filteredConvo[0].id} />
+              <Chat conversationId={filteredConvo.id} />
             </div>
-          ) : filteredConvo[0].matchId === this.props.user.id ? (
+          ) : filteredConvo.matchId === this.props.user.id ? (
             <MatchRequest
               handleAccept={this.handleAccept}
               handleReject={this.handleReject}
@@ -73,26 +75,27 @@ class disconnectedMessagesList extends Component {
             />
           ) : (
             <h4>
-              Your request to chat with {filteredConvo[0].match.firstName}
-              <img src={filteredConvo[0].match.avatar} /> is still pending!
+              Your request to chat with {filteredConvo.match.firstName}
+              <img src={filteredConvo.match.avatar} /> is still pending!
             </h4>
-          )
-        ) : null}
+          ) : null
+        }
 
         <div className="message-archive">
-          {filteredConvo.length ? (
-            filteredConvo[0].isAccepted ? (
+          {filteredConvo ?
+            filteredConvo.isAccepted ?
               <h4 id="archive-header">Messages of Old</h4>
-            ) : null
-          ) : null}
+             : null
+             : null
+         }
 
-          {filteredConvo.length ? (
-            filteredConvo[0].messages
+          {filteredConvo ?
+            filteredConvo.messages
               .filter(message => {
-                return moment(message.createdAt).isBefore(today, 'minute');
+                return moment(message.createdAt).isBefore(today, 'second');
               })
               .map(message => <Message key={message.id} message={message} />)
-          ) : (
+           : (
             <h4>Your message history is loading</h4>
           )}
         </div>
@@ -116,37 +119,3 @@ const MessagesList = withRouter(
   connect(mapStateToProps, mapDispatchToProps)(disconnectedMessagesList)
 );
 export default MessagesList;
-
-{
-  /* <h4>
-                <img className='chat-av' src={filteredConvo[0].user.avatar} /> Your request to chat from {filteredConvo[0].user.firstName} is awaiting your approval!
-                </h4>
-                <div className="match-request">
-                  <h4>Deets about {filteredConvo[0].user.firstName}:</h4>
-                  <img src={filteredConvo[0].user.photo} alt="No photo" />
-                  <p>Age: {filteredConvo[0].user.age}</p>
-                  <p>Gender: {filteredConvo[0].user.gender}</p>
-                  <p>Orientation: {filteredConvo[0].user.orientation}</p>
-                  <p>Height: {filteredConvo[0].user.height}</p>
-                  <div>
-                    <button
-                      id="accept"
-                      type="submit"
-                      onClick={evt =>
-                        this.handleAccept(evt, filteredConvo[0].id)
-                      }
-                    >
-                      Accept match request
-                    </button>
-                    <button
-                      id="reject"
-                      type="submit"
-                      onClick={evt =>
-                        this.handleReject(evt, filteredConvo[0].id)
-                      }
-                    >
-                      Decline match request
-                    </button>
-                  </div>
-                </div> */
-}
