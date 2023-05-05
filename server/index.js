fc895c02b7348a6a1a1711abf39ef5a063bf7b8a
Darshin <dparijs@readyresponders.com>
2023-05-05 15:24:01 -0400

    test: server

--- server/index.js
+++ server/index.js
@@ -12,6 +12,7 @@ const app = express();
 const socketio = require('socket.io');
 
 module.exports = app;
+app.set('port', PORT);
 
 // This is a global Mocha hook, used for resource cleanup.
 // Otherwise, Mocha v4+ never quits after tests.
